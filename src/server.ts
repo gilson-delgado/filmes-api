4bb03e2c1d52ab1cecf4048f6cd20fed220f64ef
gilson-delgado <gcdelgaa@gmail.com>
2025-03-17 11:25:24 -0300

    adicionado ordenação, genero e lingua

--- src/server.ts
+++ src/server.ts
@@ -32,10 +32,19 @@ import { PrismaClient } from "@prisma/client";
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
-app.get("/movies", async (req, res) => {
-  const movies = await prisma.movie.findMany();
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc"
+    },
+    include: {
+      genre: true,
+      language: true
+    }
+  });
   res.json(movies);
 });
 app.listen(port, () => {
   console.log(`Servidor em execução em http://localhost:${port}`);
 });
+ 
